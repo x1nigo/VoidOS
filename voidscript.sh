5b46e2e6b8f83dab2f8be5b4bc5d8f5833a40a19
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-09-15 22:36:36 +0800

    Add files via upload

--- voidscript.sh
+++ voidscript.sh
@@ -128,8 +128,8 @@ changeshell() {
 
 depower() {
 	echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/00-wheels-can-sudo
- 	rm /etc/sudoers.d/wheel # Remove the spare wheel config file
-	echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/poweroff,/usr/bin/reboot,/usr/bin/su,/usr/bin/make clean install,/usr/bin/xbps-install -Su,/usr/bin/mount,/usr/bin/umount,/usr/bin/cryptsetup,/usr/bin/simple-mtpfs,/usr/bin/fusermount" > /etc/sudoers.d/01-no-password-commands
+	rm /etc/sudoers.d/wheel # Remove the spare wheel config file
+	echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/poweroff,/usr/bin/reboot,/usr/bin/su,/usr/bin/make clean install,/usr/bin/xbps-install -Su,/usr/bin/xbps-install -S,/usr/bin/xbps-install -u,/usr/bin/mount,/usr/bin/umount,/usr/bin/cryptsetup,/usr/bin/simple-mtpfs,/usr/bin/fusermount" > /etc/sudoers.d/01-no-password-commands
 }
 
 ### Main Function ###
