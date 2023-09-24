c1a32fdc7117b7dfb81db3c0101dfcdc0dc01e71
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-09-25 02:21:28 +0800

    Update void-os.sh

--- void-os.sh
+++ void-os.sh
@@ -58,7 +58,7 @@ adduserandpass() {
 	# Adds user `$name` with password $pass1.
 	dialog --infobox "Adding user \"$name\"..." 7 50
 	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-		usermod -a -G wheel,lp,audio,video,network "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+		usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 	export repodir="/home/$name/.local/src"
 	mkdir -p "$repodir"
 	chown -R "$name":wheel "$(dirname "$repodir")"
